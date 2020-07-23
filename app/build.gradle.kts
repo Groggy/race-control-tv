57848df4d639e454b4467e522539cee880f9054b
Groggy <groggy@protonmail.com>
2020-07-23 23:43:24 +0200

    release: v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "fr.groggy.racecontrol.tv"
         minSdkVersion(26)
         targetSdkVersion(30)
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
     }
 
